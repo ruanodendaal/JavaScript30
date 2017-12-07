9a5eb5c99e1a725f2140d5a3f4365f1770f4a9ce
ruanodendaal <ruanodendaal@gmail.com>
2017-12-07 12:23:42 +0000

    completed

---
+++ key-sequence-detection/index.js
@@ -0,0 +1,14 @@
+const pressed = [];
+const secretCode = 'ruano';
+
+window.addEventListener('keyup', e => {
+  pressed.push(e.key);
+  pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
+
+  if (pressed.join('').includes(secretCode)) {
+    console.log('DING DING!!');
+    cornify_add();
+  }
+
+  console.log(pressed);
+});
